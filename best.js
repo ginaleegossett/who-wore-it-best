d9a3c6ce5ba335a401df86881b26b113458d2b70
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-10-12 22:20:34 -0700

    created files, saved photos, made user stories, set up index.html page

---
+++ best.js
@@ -0,0 +1,28 @@
+function Person(name, file) {
+	this.name = name;
+	this.file = file;
+};
+
+var articleLeft = document.createElement('img');
+	articleLeft.appendChild(document.getElementById("left"));
+	articleLeft.appendChild(this.file);
+
+
+// var imgRight = document.getElementById('right');
+
+
+var andy = new Person('Andy Lee', file: 'img/Andy.jpg');
+// var baeK = new Person('Baekyoung Song', file: 'img/Baekyoung.jpg');
+// var danny = new Person('Danny Lim', file: 'img/Danny.jpg');
+// var dongW = new Person('Dongwan Kim', file: 'img/Dongwan.jpg');
+// var eric = new Person('Eric Kim', file: 'img/Eric.jpg');
+// var heeJ = new Person('Heejun Moon', file: 'img/Heejun.jpg');
+// var hyeS = new Person('Hyesung Kim', file: 'img/Hyesung.jpg');
+// var jaeD = new Person('Jaeduc Kim', file: 'img/Jaeduc.jpeg');
+// var jaeW = new Person('Jaewon Lee', file: 'img/Jaewon.jpg');
+// var jiW = new Person('Jiwon Eun', file: 'img/Jiwon.jpg');
+// var jiY = new Person('Jiyong Koh', file: 'img/Jiyong.jpg');
+// var junJ = new Person('Junjin Park', file: 'img/Junjin.jpg');
+// var minW = new Person('Minwoo Lee', file: 'img/Minwoo.jpg');
+// var sungH = new Person('Sunghoon Kang', file: 'img/Sunghoon.jpeg');
+// var tony = new Person('Tony An', file: 'img/Tony.jpg');
---
+++ user-stories.md
@@ -0,0 +1,6 @@
+Hana, 15 - As a user, I want to see old Korean boy bands so that I can be cool.
+
+Jae, 32 - As a user, I want to go through old Korean boy band hair styles so I can try and bring back frosted tips.
+
+Ju, 21 - As a user, I want to play a fun game to distract me during studying for exams.
+
