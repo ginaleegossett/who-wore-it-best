ddb360008791513d171862957c2caa52ccddda9b
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-10-14 12:33:42 -0700

    updated with Sam's notes from code review

--- best.js
+++ best.js
@@ -1,13 +1,13 @@
 var popstars = [];
 
 //image constructor (object)
-function Person(name, src) {
-	this.name = name;
-	this.src = src;
-	popstars.push(this);
+function Person(hottie, filepath) {
+	this.cuteyName = hottie;
+	this.path = filepath;
 	this.votes = 0;
+	popstars.push(this);
 };
-//(instance that goes into the object)
+//(object instances that go into an array 'popstars')
 var andy = new Person('Andy Lee', 'img/Andy.jpeg');
 var baeK = new Person('Baekyoung Song', 'img/Baekyoung.jpg');
 var danny = new Person('Danny Lim', 'img/Danny.jpg');
@@ -27,25 +27,29 @@ var tony = new Person('Tony An', 'img/Tony.jpg');
 // randomizer (this number is being returned inside the compareImg function)
 var randImg = function() {
 	return(Math.floor(Math.random() * popstars.length));
-	console.log(randImg());
+	//the console.log isn't showing up because the return statement kicks us out of the function. If we really want to see the console.log it needs to be put outside the function.
+	//always makes notes for the console.log so you know what you're looking for
+	//console.log("the output of randImg() is " + randImg());
 };
 
 var boxLeft = document.getElementById('left');
 var boxRight = document.getElementById('right');
 
-//the rando number is being called within this function, this variable (randomPhoto1) is a number now
-//telling the random number to go to to the randomphotox variable, which then becomes part of the arary
+//the random number is being called within this function, this variable (randomPhoto1) is a number now
+//telling the random number to go to to the randomphotox variable, which then becomes part of the array
 var randomPhoto1, randomPhoto2;
 
 function compareImg () {
+	// do/while is not that common
+	// we are going to "do" randomPhoto1 and randomPhoto2 and we are going to attach random numbers to the left/right boxes, "while" the numbers are not the same
 	do {
-	randomPhoto1 = randImg();
-	randomPhoto2 = randImg();
-
-	boxLeft.src = popstars[randomPhoto1].src;
-	boxRight.src = popstars[randomPhoto2].src;
-}
-while (randomPhoto1 === randomPhoto2);
+		randomPhoto1 = randImg();
+		randomPhoto2 = randImg();
+	// this.src is referring to the HTML attribute, since it is not called in the html (only img is), would be better to change the .src after popstars[randomPhoto1/2] to .path
+		boxLeft.src = popstars[randomPhoto1].path;
+		boxRight.src = popstars[randomPhoto2].path;
+	}
+	while (randomPhoto1 === randomPhoto2);
 }
 
 //calling the function to compare the photos
@@ -53,14 +57,16 @@ compareImg();
 
 //this is a callback function(the function then {} compareImg();), it allows multiple methods to be excuted inside this function
 boxLeft.addEventListener('click', function() {
-	popstars[randomPhoto1].votes +=1;
-	console.log(popstars[randomPhoto1].name + " has " + popstars[randomPhoto1].votes + " votes");
+	popstars[randomPhoto1].votes += 1;
+	console.log(popstars[randomPhoto1].cuteyName + " has " + popstars[randomPhoto1].votes + " votes");
 	compareImg();
 });
 
 boxRight.addEventListener('click',function() {
-	popstars[randomPhoto2].votes +=1;
-	console.log(popstars[randomPhoto2].name + " has " + popstars[randomPhoto2].votes + " votes");
+	popstars[randomPhoto2].votes += 1;
+	console.log(popstars[randomPhoto2].cuteyName + " has " + popstars[randomPhoto2].votes + " votes");
 	compareImg();
 });
+
+
 
