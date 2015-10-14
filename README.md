0c7c40baf0e4fba65b590482e01b5b569037f57b
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-10-13 19:20:23 -0700

    in case of fire, pushing

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Mon, October 12, 2015
 - Found, edited and saved all the hairstyle photos
 - Created index.html and stylized the page using CSS
 - Started work on the JS file
+
+Tues, October 13, 2015
+- Got my two photos pushed to index.html
+- got randomizer to work
+-tallied photo votes in console.log
--- best.js
+++ best.js
@@ -1,28 +1,71 @@
-function Person(name, file) {
+var popstars = [];
+
+//image constructor (object)
+function Person(name, src) {
 	this.name = name;
-	this.file = file;
+	this.src = src;
+	popstars.push(this);
+	this.votes = 0;
+};
+//(instance that goes into the object)
+var andy = new Person('Andy Lee', 'img/Andy.jpeg');
+var baeK = new Person('Baekyoung Song', 'img/Baekyoung.jpg');
+var danny = new Person('Danny Lim', 'img/Danny.jpg');
+var dongW = new Person('Dongwan Kim', 'img/Dongwan.jpg');
+var eric = new Person('Eric Kim', 'img/Eric.jpg');
+var heeJ = new Person('Heejun Moon', 'img/Heejun.jpg');
+var hyeS = new Person('Hyesung Kim', 'img/Hyesung.jpg');
+var jaeD = new Person('Jaeduc Kim', 'img/Jaeduc.jpeg');
+var jaeW = new Person('Jaewon Lee', 'img/Jaewon.jpg');
+var jiW = new Person('Jiwon Eun', 'img/Jiwon.jpg');
+var jiY = new Person('Jiyong Koh', 'img/Jiyong.jpg');
+var junJ = new Person('Junjin Park', 'img/Junjin.jpg');
+var minW = new Person('Minwoo Lee', 'img/Minwoo.jpg');
+var sungH = new Person('Sunghoon Kang', 'img/Sunghoon.jpeg');
+var tony = new Person('Tony An', 'img/Tony.jpg');
+
+//tracker should be an object....
+// var tracker() = {}
+
+//randomizer (this number is being returned inside the compareImg function)
+var randImg = function() {
+	return(Math.floor(Math.random() * popstars.length));
+	console.log(randImg());
 };
 
-var articleLeft = document.createElement('img');
-	articleLeft.appendChild(document.getElementById("left"));
-	articleLeft.appendChild(this.file);
+var boxLeft = document.getElementById('left');
+var boxRight = document.getElementById('right');
 
 
-// var imgRight = document.getElementById('right');
+//the rando number is being called within this function, this variable (randomPhoto1) is a number now
+//telling the random number to go to to the randomphotox variable, which then becomes part of the arary
+var randomPhoto1, randomPhoto2;
+
+function compareImg () {
+	do {
+	randomPhoto1 = randImg();
+	randomPhoto2 = randImg();
+
+	boxLeft.src = popstars[randomPhoto1].src;
+	boxRight.src = popstars[randomPhoto2].src;
+}
+while (randomPhoto1 === randomPhoto2);
+}
+
+//calling the function to compare the photos
+compareImg();
 
 
-var andy = new Person('Andy Lee', file: 'img/Andy.jpg');
-// var baeK = new Person('Baekyoung Song', file: 'img/Baekyoung.jpg');
-// var danny = new Person('Danny Lim', file: 'img/Danny.jpg');
-// var dongW = new Person('Dongwan Kim', file: 'img/Dongwan.jpg');
-// var eric = new Person('Eric Kim', file: 'img/Eric.jpg');
-// var heeJ = new Person('Heejun Moon', file: 'img/Heejun.jpg');
-// var hyeS = new Person('Hyesung Kim', file: 'img/Hyesung.jpg');
-// var jaeD = new Person('Jaeduc Kim', file: 'img/Jaeduc.jpeg');
-// var jaeW = new Person('Jaewon Lee', file: 'img/Jaewon.jpg');
-// var jiW = new Person('Jiwon Eun', file: 'img/Jiwon.jpg');
-// var jiY = new Person('Jiyong Koh', file: 'img/Jiyong.jpg');
-// var junJ = new Person('Junjin Park', file: 'img/Junjin.jpg');
-// var minW = new Person('Minwoo Lee', file: 'img/Minwoo.jpg');
-// var sungH = new Person('Sunghoon Kang', file: 'img/Sunghoon.jpeg');
-// var tony = new Person('Tony An', file: 'img/Tony.jpg');
+//this is a callback function(the function then {} compareImg();)
+boxLeft.addEventListener('click', function() {
+	popstars[randomPhoto1].votes +=1;
+	console.log(popstars[randomPhoto1].name + " has " + popstars[randomPhoto1].votes + " votes");
+	compareImg();
+});
+
+boxRight.addEventListener('click',function() {
+	popstars[randomPhoto2].votes +=1;
+	console.log(popstars[randomPhoto2].name + " has " + popstars[randomPhoto2].votes + " votes");
+	compareImg();
+});
+
